c2a14b5665306551dfe947fcce69e1aca542ddff
Ahmet Kenan Algül <akenanalgul@gmail.com>
2024-12-08 23:43:40 +0300

    Deneme

---
+++ src/main/java/com/example/BAMS/BamsApplication.java
@@ -0,0 +1,13 @@
+package com.example.BAMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BAMS/BamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BAMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
