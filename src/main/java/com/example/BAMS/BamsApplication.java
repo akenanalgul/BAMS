949e5a711807d1ea28637eab77e9b79ed2f1ff8e
Ahmet Kenan Algül <akenanalgul@gmail.com>
2024-12-12 02:53:50 +0300

    Ignore .env file

--- src/main/java/com/example/BAMS/BamsApplication.java
+++ src/main/java/com/example/BAMS/BamsApplication.java
@@ -1,5 +1,5 @@
 package com.example.BAMS;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BamsApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_USERNAME",dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD",dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_URL",dotenv.get("DB_URL"));
+
+
 		SpringApplication.run(BamsApplication.class, args);
 	}
 
