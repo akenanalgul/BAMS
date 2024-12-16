d12047f18739ba56f598f1f42760d077f1992047
Ahmet Kenan Algül <akenanalgul@gmail.com>
2024-12-16 18:26:29 +0300

    Transactions getter & setter

---
+++ src/main/java/com/example/BAMS/model/Transactions.java
@@ -0,0 +1,69 @@
+package com.example.BAMS.model;
+
+import jakarta.persistence.*;
+
+import java.nio.DoubleBuffer;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "transactions")
+
+public class Transactions {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id",nullable = false)
+    private Account account;
+
+    @Column(nullable = false)
+    private String transactionType;
+
+    @Column(nullable = false)
+    private Double amount;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public Account getAccount(){
+        return account;
+    }
+    public void setAccount(Account account){
+        this.account= account;
+    }
+
+
+    public String getTransactionType(){
+        return transactionType;
+    }
+    public void setTransactionType(String transactionType){
+        this.transactionType=transactionType;
+    }
+
+    public Double getAmount(){
+        return amount;
+    }
+    public void setAmount(Double amount){
+        this.amount=amount;
+    }
+
+
+    public LocalDateTime getCreatedAt(){
+        return createdAt;
+    }
+    public void setCreatedAt(LocalDateTime createdAt){
+        this.createdAt=createdAt;
+    }
+}
