35759897e1ef4a93312f8877dcfadcf78e292c66
Ahmet Kenan Algül <akenanalgul@gmail.com>
2025-01-31 16:26:19 +0300

    adding login and authService

--- src/main/java/com/example/BAMS/controller/AuthController.java
+++ src/main/java/com/example/BAMS/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.example.BAMS.controller;
 
+import com.example.BAMS.dto.LoginRequestDTO;
 import com.example.BAMS.dto.RegisterRequestDTO;
 import com.example.BAMS.model.Role;
 import com.example.BAMS.model.User;
 import com.example.BAMS.repository.UserRepository;
+import com.example.BAMS.security.JwtUtil;
+import com.example.BAMS.service.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +17,12 @@ public class AuthController {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final AuthService authService;
 
-    public AuthController(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+    public AuthController(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder,AuthService authService) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.authService=authService;
     }
 
     @PostMapping("/register")
@@ -35,4 +40,22 @@ public class AuthController {
         userRepository.save(user);
         return ResponseEntity.ok("User registered successfully");
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequestDTO request) {
+        String token = authService.login(request);
+        return ResponseEntity.ok(token);
+    }
+    /*
+        User user = userRepository.findByEmail(request.getEmail());
+
+        if (user == null || !passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            return ResponseEntity.badRequest().body("Invalid credentials");
+        }
+
+        String token = JwtUtil.generateToken(user.getEmail());
+        return ResponseEntity.ok(token);
+
+     */
+
 }
